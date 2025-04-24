0331ed62e3f2e4317a6bb3b13a340b4856a2b92b
Esha Patel <Patellesha11@gmail.com>
2025-04-24 13:04:44 +0300

    added

---
+++ Library_Management_System/main.py
@@ -0,0 +1,71 @@
+class Book: 
+    def __init__(self, title, author, isbn):
+        self.__title = title
+        self.__author = author
+        self.__isbn = isbn
+        self.__is_available = True
+
+    def get_info(self):
+        return f"{self.__title} by {self.__author}"
+    
+    def is_available(self):
+        return self.__is_available
+    
+    def borrow(self):
+        if self.__is_available:
+            self.__is_available = False
+            print(f"📕 Borrowed: {self.__title}")
+        else:
+            print("Book is already borrowed.")
+
+    def return_book(self):
+        self.__is_available = True
+        print(f"  Returned: {self.__title}")
+
+    class Textbook(Book):
+        def __init__(self, title, author, isbn, subject):
+            super().__init__(title, author, isbn)
+            self.__subject = subject
+
+    
+        def get_subject(self):
+            return self.__subject
+        
+    class Member:
+        def __init__(self, name, member_id):
+            self._name = name
+            self._member_id = member_id
+            self._borrowed_books = []
+
+        def  get_name(self):
+            return self._name
+        
+    class Student(Member):
+        def __init__(self, name, member_id):
+            super().__init__(name, member_id)
+            self._borrow_limit = 3
+
+    class Faculty(Member):
+        def __init__(self, name, member_id):
+            super().__init__(name, member_id)
+            self._borrow_limit = 5
+
+    class Staff:
+        def __init__ (self, name, staff_id):
+           self._name = name
+           self._staff_id = staff_id
+
+    class  Librarian(Staff):
+        def __init__(self,  name, staff_id):
+            super().__init__(name, staff_id)
+
+        def add_book(self,library, book):
+            library.books.append(book)
+            print(f" '{book.get.info()}'added to Library.")
+    
+
+
+
+
+            
+
