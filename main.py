8d209be10068925882e45b560705f47d519a3eaf
Esha Patel <Patellesha11@gmail.com>
2025-04-24 12:05:28 +0300

    added is self available

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ class Book:
         self.__author = author
         self.__isbn = isbn
         self.__is_available = True
+
+    def get_info(self):
+        return f"{self.__title} by {self.__author}"
+    
+    def is_available(self):
+        return self.__is_available
